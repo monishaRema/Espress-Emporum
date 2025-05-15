d195877b1b3df5453a1ff8ba01fe0474a488a7bb
monisharema <monisharema97@gmail.com>
2025-05-15 19:15:45 +0300

    setup database connection and env

--- server/index.js
+++ server/index.js
@@ -1,16 +1,42 @@
-const express = require('express');
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
-const cors = require('cors');
 const port = process.env.PORT || 3000;
-
 
 app.use(cors());
 app.use(express.json());
 
- app.get('/',(req,res)=>{
-    res.send('cofferr is on the way')
- })
 
- app.listen(port,()=>{
-    console.log('servser is running')
- })
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@remadb.w7lg8gq.mongodb.net/?retryWrites=true&w=majority&appName=remaDb`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("cofferr is on the way");
+});
+
+app.listen(port, () => {
+  console.log("server is running");
+});
